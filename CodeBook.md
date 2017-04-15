d3bbf0a799afd9aa0d615d0a4cb88aba1ee2a999
nancyirisarri <nancyirisarri@gmail.com>
2017-04-16 01:18:04 +0200

    Initial commit.

---
+++ CodeBook.md
@@ -0,0 +1,82 @@
+Processed Data 
+==============
+
+Uses descriptive activity names to name the activities in the data set. Appropriately labels each of the columns
+with descriptive variable names. These have been renamed to make more human readable.
+
+Extracts only the measurements on the mean and standard deviation for each of the measurements described in the 
+Feature Selection. This merged data set contains the test data in the upper rows (1 to 2947) and the training data in the 
+lower rows (2948 to 10299). Columns 1 and 2 contain the subject and activity values, respectively. Columns 3 to 81 contain 
+the exctracted measurements.
+
+The mean of each of the extracted measurements is calculated using the mean() function in R. This averaged data set contains
+rows for each of the means. Columns 1 to 6 are for each of the activity types. Columns 7 to 36 are for each of the subjects.
+
+Feature Selection 
+=================
+
+The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals 
+tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant 
+rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter 
+with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated 
+into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass 
+Butterworth filter with a corner frequency of 0.3 Hz. 
+
+Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals 
+(tBodyAccJerk-XYZ and tBodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated 
+using the Euclidean norm (tBodyAccMag, tGravityAccMag, tBodyAccJerkMag, tBodyGyroMag, tBodyGyroJerkMag). 
+
+Finally a Fast Fourier Transform (FFT) was applied to some of these signals producing fBodyAcc-XYZ, fBodyAccJerk-XYZ, 
+fBodyGyro-XYZ, fBodyAccJerkMag, fBodyGyroMag, fBodyGyroJerkMag. (Note the 'f' to indicate frequency domain signals). 
+
+These signals were used to estimate variables of the feature vector for each pattern:  
+'-XYZ' is used to denote 3-axial signals in the X, Y and Z directions.
+
+tBodyAcc-XYZ
+tGravityAcc-XYZ
+tBodyAccJerk-XYZ
+tBodyGyro-XYZ
+tBodyGyroJerk-XYZ
+tBodyAccMag
+tGravityAccMag
+tBodyAccJerkMag
+tBodyGyroMag
+tBodyGyroJerkMag
+fBodyAcc-XYZ
+fBodyAccJerk-XYZ
+fBodyGyro-XYZ
+fBodyAccMag
+fBodyAccJerkMag
+fBodyGyroMag
+fBodyGyroJerkMag
+
+The set of variables that were estimated from these signals are: 
+
+mean(): Mean value
+std(): Standard deviation
+mad(): Median absolute deviation 
+max(): Largest value in array
+min(): Smallest value in array
+sma(): Signal magnitude area
+energy(): Energy measure. Sum of the squares divided by the number of values. 
+iqr(): Interquartile range 
+entropy(): Signal entropy
+arCoeff(): Autorregresion coefficients with Burg order equal to 4
+correlation(): correlation coefficient between two signals
+maxInds(): index of the frequency component with largest magnitude
+meanFreq(): Weighted average of the frequency components to obtain a mean frequency
+skewness(): skewness of the frequency domain signal 
+kurtosis(): kurtosis of the frequency domain signal 
+bandsEnergy(): Energy of a frequency interval within the 64 bins of the FFT of each window.
+angle(): Angle between to vectors.
+
+Additional vectors obtained by averaging the signals in a signal window sample. 
+These are used on the angle() variable:
+
+gravityMean
+tBodyAccMean
+tBodyAccJerkMean
+tBodyGyroMean
+tBodyGyroJerkMean
+
+The complete list of variables of each feature vector is available in 'features.txt'
---
+++ run_analysis.R
@@ -0,0 +1,203 @@
+# Functions for merging test and training data sets. 
+
+getPath <- function(yourPath) {
+  # yourPath: optional variable if the unzipped directory containing the files 
+  # to process is not in the same location as the .R file.
+  #
+  # Function to get the path to the files.
+  #
+  # Return character vector.
+  
+  path <- paste("UCI HAR Dataset", sep=.Platform$file.sep)
+  
+  if (!missing(yourPath)) {
+    path <- paste(yourPath, "UCI HAR Dataset", sep=.Platform$file.sep)
+  } 
+  
+  path
+}
+
+mergeSubjects <- function(yourPath) {
+  # yourPath: optional variable if the unzipped directory containing the files 
+  # to process is not in the same location as the .R file.
+  #
+  # Extracts the subject numbers from subject_test.txt and subject_train.txt 
+  # into the column 'subject'. Then merges into one column.
+  #
+  # Return data.frame
+  
+  path <- getPath(yourPath)
+  
+  subject_test <- read.table(
+    paste(path, "test", "subject_test.txt", sep=.Platform$file.sep), 
+    col.names="subject")
+  
+  subject_train <- read.table(
+    paste(path, "train", "subject_train.txt", sep=.Platform$file.sep), 
+    col.names="subject")
+  
+  rbind(subject_test, subject_train, deparse.level=0)
+}
+
+renameFeatures <- function(yourPath) {
+  # yourPath: optional variable if the unzipped directory containing the files 
+  # to process is not in the same location as the .R file.
+  #
+  # Rename the rows in features.txt to make more human readable. Keep only the 
+  # rows where the column has either 'mean' or 'std', and uppercase the first 
+  # letter.
+  #
+  # Return data.frame
+  
+  path <- getPath(yourPath)
+  
+  features <- read.table(paste(path, "features.txt", sep=.Platform$file.sep), 
+                         col.names = c("id", "feature"))
+
+  features$feature <- sapply(features$feature, function(x) { 
+    if (grepl("mean", x) | grepl("std", x)) {
+      x <- sub("\\(\\)-", "", x) 
+      x <- sub("-", "", x)
+      x <- sub("\\(\\)", "", x)
+      x <- sub("mean", "Mean", x)
+      x <- sub("std", "Std", x)
+    }
+  })
+  
+  features[grepl("Mean", features$feature) | grepl("Std", features$feature),]
+}
+
+mergeObservations <- function(yourPath) {
+  # yourPath: optional variable if the unzipped directory containing the files 
+  # to process is not in the same location as the .R file.
+  #
+  # Extract the relevant columns from X_test.txt and X_train.txt. Name the 
+  # columns using the names from renameFeatures(). Then merge into one data 
+  # frame.
+  #
+  # Return data.frame
+  
+  path <- getPath(yourPath)
+  
+  observations_test <- read.table(
+    paste(path, "test", "X_test.txt", sep=.Platform$file.sep), header = FALSE)
+
+  observations_train <- read.table(
+    paste(path, "train", "X_train.txt", sep=.Platform$file.sep), header = FALSE)
+
+  # Extract the relevant columns using the indices from renameFeatures().
+  features <- renameFeatures()
+  observations_test <- observations_test[, features[,1]]
+  observations_train <- observations_train[, features[,1]]
+  
+  observations <- data.frame(
+    rbind(observations_test, observations_train, deparse.level = 0))
+  
+  colnames(observations) <- features$feature
+  
+  observations
+}
+
+mergeActivities <- function(yourPath) {
+  # yourPath: optional variable if the unzipped directory containing the files 
+  # to process is not in the same location as the .R file.
+  #
+  # Convert activity numbers in y_test.txt and y_train.txt into words using the 
+  # mapping in activity_labels.txt. Name the column as 'activity'.
+  #
+  # Return data.frame
+
+  path <- getPath(yourPath)
+  
+  # Store the labels.
+  activity_labels <- read.table(
+    paste(path, "activity_labels.txt", sep = .Platform$file.sep),
+    col.names = c("id", "activity"))
+  
+  # Get the data.
+  activities_train <- read.table(
+    paste(path, "train", "y_train.txt", sep = .Platform$file.sep),
+    col.names = c("activity"))
+  
+  activities_test <- read.table(
+    paste(path, "test", "y_test.txt", sep = .Platform$file.sep), 
+    col.names = c("activity"))
+  
+  # Do the mapping.
+  for (i in 1:nrow(activity_labels)) {
+    
+    activities_train$activity <- sapply(
+      activities_train$activity, 
+      function(x) { x <- sub(activity_labels[i,1], activity_labels[i,2], x) })
+    
+    activities_test$activity <- sapply(
+      activities_test$activity, 
+      function(x) { x <- sub(activity_labels[i,1], activity_labels[i,2], x) })
+  }
+  
+  rbind(activities_test, activities_train, deparse.level = 0)
+}
+
+mergeAll <- function(yourPath) {
+  # Merge the test and training data. The upper rows contain the test data while 
+  # the lower rows the training data.
+  # Return data.frame
+  
+  subjects <- mergeSubjects(yourPath)
+  activities <- mergeActivities(yourPath)
+  observations <- mergeObservations(yourPath)
+
+  cbind(subjects, activities, observations)
+}
+
+mergeAverages <- function(yourPath) {
+  # From the merged data, creates an independent tidy data set with the average 
+  # of each variable for each activity and each subject. The rows indicate the 
+  # variables and the columns the activities and subjects.
+  #
+  # Return data.frame
+  
+  require(dplyr)
+  
+  observations <- mergeAll(yourPath)
+  
+  # Use these for the column names.
+  activity_labels <- sort(unique(observations$activity))
+  subject_labels <- sort(unique(observations$subject))
+  
+  variables <- colnames(observations)
+  
+  # Data frame to hold the object to return.
+  averages <- data.frame(row.names=variables[3:ncol(observations)])
+  
+  for (i in 3:ncol(observations)) {
+      
+    for (j in seq_along(activity_labels)) {
+      subobservations <- filter(observations, activity==activity_labels[j])
+      averages[i-2,j] <- mean(subobservations[,i], na.rm=TRUE)
+    }
+
+    for (k in seq_along(subject_labels)) {
+      subobservations <- filter(observations, subject==subject_labels[k])
+      averages[i-2,k+length(activity_labels)] <- mean(subobservations[,i], 
+                                                      na.rm=TRUE)
+    }
+    
+  }
+  
+  colnames(averages) <- c(activity_labels, subject_labels)
+  
+  averages
+}
+
+writeOutput <- function(filenameMerged, filenameAverages) {
+  # filename: name of file in which to write the data.
+  
+  # Function to write the merged data and the averaged data to files.
+  
+  require(data.table)
+  
+  fwrite(mergeAll(), filenameMerged)
+  
+  fwrite(mergeAverages(), filenameAverages)
+}
